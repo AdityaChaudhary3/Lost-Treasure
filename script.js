4ed41fea5d7c9db3cc2fe05611f7310fe51a8f60
Aditya Chaydhary <87495387+AdityaChaudhary3@users.noreply.github.com>
2022-07-18 00:30:11 +0530

    first commit
    just start a new repo.

---
+++ script.js
@@ -0,0 +1,39 @@
+alert("savdhan");
+
+let box1 = document.getElementById("Box1")
+box1.addEventListener('click',function run(){
+    confirm("You Lost")
+});
+let box2 = document.getElementById("Box2")
+box2.addEventListener('click',function run2(){
+    confirm("You Lost")
+});
+let box3 = document.getElementById("Box3")
+box3.addEventListener('click',function run3(){
+    confirm(" Congurates! You Win .....")
+});
+let box4 = document.getElementById("Box4")
+box4.addEventListener('click',function run4(){
+    confirm("You Lost")
+});
+let box5 = document.getElementById("Box5")
+box5.addEventListener('click',function run5(){
+    confirm("You Lost")
+});
+let box6 = document.getElementById("Box6")
+box6.addEventListener('click',function run6(){
+    confirm("You Lost")
+});
+let box7 = document.getElementById("Box7")
+box7.addEventListener('click',function run7(){
+    confirm("You Lost")
+});
+let box8 = document.getElementById("Box8")
+box8.addEventListener('click',function run8(){
+    confirm("You Lost")
+});
+let box9 = document.getElementById("Box9")
+box9.addEventListener('click',function run9(){
+    confirm("You Lost")
+});
+
