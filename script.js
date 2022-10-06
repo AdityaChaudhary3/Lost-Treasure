5ba31099459984ce6f903cb3a1553ea433dce2f8
Dany Cervantes <danycervantesq@gmail.com>
2022-10-06 01:07:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,53 @@
-//Once I know how I'll try to get the chest to move around randomly.
+let box1 = document.getElementById("1");
+let box2 = document.getElementById("2");
+let box3 = document.getElementById("3");
+let box4 = document.getElementById("4");
+let box5 = document.getElementById("5");
+let box6 = document.getElementById("6");
+let box7 = document.getElementById("7");
+let box8 = document.getElementById("8");
+let box9 = document.getElementById("9");
 
-let box1 = document.getElementById("Box1")
-box1.addEventListener('click',function run(){
-    confirm("You Lost")
-});
-let box2 = document.getElementById("Box2")
-box2.addEventListener('click',function run2(){
-    confirm("You Lost")
-});
-let box3 = document.getElementById("Box3")
-box3.addEventListener('click',function run3(){
-    confirm(" Congurates! You Win .....")
-});
-let box4 = document.getElementById("Box4")
-box4.addEventListener('click',function run4(){
-    confirm("You Lost")
-});
-let box5 = document.getElementById("Box5")
-box5.addEventListener('click',function run5(){
-    confirm("You Lost")
-});
-let box6 = document.getElementById("Box6")
-box6.addEventListener('click',function run6(){
-    confirm("You Lost")
-});
-let box7 = document.getElementById("Box7")
-box7.addEventListener('click',function run7(){
-    confirm("You Lost")
-});
-let box8 = document.getElementById("Box8")
-box8.addEventListener('click',function run8(){
-    confirm("You Lost")
-});
-let box9 = document.getElementById("Box9")
-box9.addEventListener('click',function run9(){
-    confirm("You Lost")
-});
+box1.onclick = function() { selection(1) };
+box2.onclick = function() { selection(2) };
+box3.onclick = function() { selection(3) };
+box4.onclick = function() { selection(4) };
+box5.onclick = function() { selection(5) };
+box6.onclick = function() { selection(6) };
+box7.onclick = function() { selection(7) };
+box8.onclick = function() { selection(8) };
+box9.onclick = function() { selection(9) };
+
+let treasure = randomRange(1,9);
+
+//Returns a random number within a chosen range
+function randomRange(min, max) { 
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+//Math.floor() rounds down to the nearest whole number  e.i. 10 = 0 - 9  
+//Math.random() returns a random decimal between 0 - 0.99
+}
+
+
+function selection(pick) { 
+    
+    let lose = document.querySelector(".lose");
+    let win = document.querySelector(".win");
+
+    if (pick == treasure) {
+
+        win.classList.remove("hide");
+        setTimeout(function() { win.classList.add("hide") }, 15000);
+        treasure = randomRange(1,9);
+
+    } else {
+        
+        lose.classList.remove("hide");
+        setTimeout(function() { lose.classList.add("hide") }, 2500 );
+    }
+
+}
+
+
+
 
